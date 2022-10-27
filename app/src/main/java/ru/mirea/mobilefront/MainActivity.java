0bdac2ca5fa2f9a8ca0dbf4d41bbfe0d8a35e7b1
IShorin <ishorin@icloud.com>
2022-10-27 16:18:42 +0300

    Addition commit for animations

--- app/src/main/java/ru/mirea/mobilefront/MainActivity.java
+++ app/src/main/java/ru/mirea/mobilefront/MainActivity.java
@@ -3,15 +3,27 @@ package ru.mirea.mobilefront;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button testforanim;//переменная для id кнопки (для анимации)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        testforanim=(Button)findViewById(R.id.switch_remember);//получаем id кнопочки
+        testforanim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation An1 = AnimationUtils.loadAnimation(MainActivity.this,R.anim.bounce);//Получаем анимацию
+                testforanim.startAnimation(An1);//Запуск анимации кнопки
+            }
+        });
 
 
 
