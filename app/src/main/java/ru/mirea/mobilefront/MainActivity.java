4a4d88e7c31e459b56221436814e63e678e2ddcd
IShorin <ishorin@icloud.com>
2022-11-20 01:34:58 +0300

    glide test 2

--- app/src/main/java/ru/mirea/mobilefront/MainActivity.java
+++ app/src/main/java/ru/mirea/mobilefront/MainActivity.java
@@ -1,49 +1,39 @@
 package ru.mirea.mobilefront;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
-import android.animation.ValueAnimator;
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import com.bumptech.glide.Glide;
 
-import lombok.SneakyThrows;
-
-import ru.mirea.mobilefront.dto.LoginFormDto;
-import ru.mirea.mobilefront.dto.RoleEnum;
-import ru.mirea.mobilefront.dto.UserDto;
+import pl.droidsonroids.gif.GifImageView;
 import ru.mirea.mobilefront.service.AuthService;
 import ru.mirea.mobilefront.service.UserService;
-import ru.mirea.mobilefront.service.UserSession;
 
 public class MainActivity extends AppCompatActivity {
     //Button loginButton;
     AppCompatButton loginButton;
     EditText loginField;
     EditText passwordField;
+    GifImageView gifImageView; //тестовое поле
 
     UserService userService = new UserService();
     AuthService authService = new AuthService();
 
-    @SuppressLint("SetTextI18n")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Intent intent = new Intent(this, MenuActivity.class);
         //startActivity(intent);
-
-
-
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -55,12 +45,18 @@ public class MainActivity extends AppCompatActivity {
         loginField=(EditText)findViewById(R.id.login_text);
         passwordField=(EditText)findViewById(R.id.password_text);
         loginButton = (AppCompatButton)findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
                 authService.login(loginField.getText().toString()
                         ,passwordField.getText().toString());
-              //  Здесь врубается кнопка логина, загрузочка все такое ее надо будет вырубить
+
+                Glide with(this)
+                        .load()
+
+                //  Здесь врубается кнопка логина, загрузочка все такое ее надо будет вырубить
                 //  через секунд 10 в случае безуспешности логина и все такое
                 //Старт анимации!!!
             }
